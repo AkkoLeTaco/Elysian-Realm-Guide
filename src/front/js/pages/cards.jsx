af905db2e1bf23054954571464977ca36ae41574
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-20 19:03:33 +0000

    made changes

--- src/front/js/pages/cards.jsx
+++ src/front/js/pages/cards.jsx
@@ -3,7 +3,9 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
+
 export const Cards = () => {
+  const { store, actions } = useContext(Context);
   return (
     <div className="list-group-horizontal row row-cols-1 row-cols-md-9 g-1 mx-2 mb-3">
       <div className="card">
@@ -18,6 +20,26 @@ export const Cards = () => {
             Go somewhere
           </a>
         </div>
+      </div>
+      <div className="list-group">
+        {store.list &&
+          store.list.map((item, index) => {
+            return (
+              <div key={index}>
+                <div>{item.name}</div>
+                <div>
+                  <img
+                    src="https://i.ibb.co/1KwRggx/falu-red-color-solid-background-1920x1080.png"
+                    alt="falu-red-color-solid-background-1920x1080"
+                    border="0"
+                  />
+                </div>
+                <div {...item.desciption}></div>
+                <div {...item.best_signets}></div>
+                <div {...item.best_weapon}></div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
