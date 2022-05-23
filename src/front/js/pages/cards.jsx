529766ebc822634de1c49b7d2c6e64c38ba04890
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-23 16:32:01 +0000

    made changes

--- src/front/js/pages/cards.jsx
+++ src/front/js/pages/cards.jsx
@@ -5,37 +5,12 @@ import "../../styles/home.css";
 import { Context } from "../store/appContext";
 export const Cards = () => {
   const { store, actions } = useContext(Context);
+  console.log(store);
   return (
     <>
-      {store.CharacterCards.results &&
-        store.CharacterCards.results.map((ele, index) => {
-          return (
-            <div
-              key={index}
-              className="row row-cols-1 row-cols-md-9 g-1 mx-2 mb-3"
-            >
-              <div className="card" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={store.img + ele.uid + ".png"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{ele.name}</h5>
-                  <Link to={"characters/" + ele.uid}>
-                    <button
-                      className="btn btn-primary px-2.5"
-                      onClick={() => {
-                        actions.loadDetails(ele.uid);
-                      }}
-                    >
-                      Learn more
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
+      {store.list &&
+        store.list.map((ele, index) => {
+          return <div>{ele.name}</div>;
         })}
     </>
   );
