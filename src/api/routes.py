66c06530c70acbbc1e182016e8b09585f40f26ce
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-05-25 14:19:49 +0000

    app.py and routes.py fixed

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,14 +16,6 @@ api = Blueprint('api', __name__)
 def sitemap():
     return generate_sitemap(app)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
 
 @api.route('/user', methods=['GET'])
 def handle_hello():
