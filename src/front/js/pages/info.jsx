e87c52415e5cc3902826f334683832f8ab68d2cd
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-26 16:34:36 +0000

    made changes

--- src/front/js/pages/info.jsx
+++ src/front/js/pages/info.jsx
@@ -9,7 +9,21 @@ export const Info = (props) => {
     <>
       {store.user ? (
         <>
-          <div></div>
+          <div className="card mb-3">
+            <img
+              src="https://i.ibb.co/Y0PR9py/0473aff9e8a10ed9f3e01159a538e9ee-2171723955283249678.png"
+              alt="0473aff9e8a10ed9f3e01159a538e9ee-2171723955283249678"
+              border="0"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Welcome Captain,</h5>
+              <p className="card-text">
+                To the Elysian Realm Guide! Here, we will instruct you on the
+                best characters, signets, and weapons that will aid you in your
+                success!
+              </p>
+            </div>
+          </div>
         </>
       ) : (
         <Login />
