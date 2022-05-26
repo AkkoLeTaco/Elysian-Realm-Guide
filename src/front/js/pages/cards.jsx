ca57d3622e3fe708e6db3943e644f994ff457700
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-26 14:59:51 +0000

    made changes

--- src/front/js/pages/cards.jsx
+++ src/front/js/pages/cards.jsx
@@ -5,22 +5,19 @@ export const Cards = () => {
   const { store, actions } = useContext(Context);
   console.log(store);
   return (
-    <>
+    <div className="card-group">
       {store.list &&
         store.list.map((ele, index) => {
           return (
-            <>
-              <div>{ele.name}</div>
-              <div className="card" style={{ width: "18rem" }}>
-                <img src={ele.image} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">{ele.name}</h5>
-                  <p className="card-text">{ele.description}</p>
-                </div>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={ele.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{ele.name}</h5>
+                <p className="card-text">{ele.description}</p>
               </div>
-            </>
+            </div>
           );
         })}
-    </>
+    </div>
   );
 };
