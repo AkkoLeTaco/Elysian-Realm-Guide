999ec7e71f03083e766e7e962feae56609b06981
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-05-26 15:00:14 +0000

    changes

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -2,21 +2,24 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import ElysiaSignet from "../../img/Elysia_Signet.png";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   return (
     <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-      <a className="navbar-brand" href="/">
-        <img
-          src={ElysiaSignet}
-          alt=""
-          width="30"
-          height="24"
-          className="d-inline-block align-text-top"
-        />{" "}
-        <span className="title-color">Elysian Realm Guide</span>
-      </a>
+      <Link to="/">
+        <span className="navbar-brand">
+          <img
+            src={ElysiaSignet}
+            alt=""
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />{" "}
+          <span className="title-color">Elysian Realm Guide</span>
+        </span>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -29,21 +32,15 @@ export const Navbar = () => {
         {store.user ? (
           <>
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/info">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/characters">
-                  Characters
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/bosses">
-                  Boss Info
-                </a>
-              </li>
+              <Link to="/info">
+                <li className="nav-item nav-link">About</li>
+              </Link>
+              <Link to="/characters">
+                <li className="nav-item nav-link">Characters</li>
+              </Link>
+              <Link to="/bosses">
+                <li className="nav-item nav-link">Boss Info</li>
+              </Link>
               <li className="nav-item">
                 <a
                   className="nav-link"
