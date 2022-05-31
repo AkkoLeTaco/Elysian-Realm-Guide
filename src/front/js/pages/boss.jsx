575c556227d989cdea990cf91ab59a9325370979
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-05-31 14:24:13 +0000

    finishing touches

--- src/front/js/pages/boss.jsx
+++ src/front/js/pages/boss.jsx
@@ -9,81 +9,89 @@ export const Boss = (props) => {
     <>
       {store.user ? (
         <>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div className="card mb-3" style={{ width: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..." />
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
                   </p>
-                  <p class="card-text">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div className="card mb-3" style={{ width: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..." />
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
                   </p>
-                  <p class="card-text">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div className="card mb-3" style={{ width: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..." />
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
                   </p>
-                  <p class="card-text">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div className="card mb-3" style={{ maxWidth: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="..." className="img-fluid rounded-start" alt="..." />
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
                   </p>
-                  <p class="card-text">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
                   </p>
                 </div>
               </div>
--- src/front/js/pages/info.jsx
+++ src/front/js/pages/info.jsx
@@ -9,29 +9,27 @@ export const Info = () => {
     <>
       {store.user ? (
         <>
-          <div className="card mb-3" style={{ height: "18rem" }}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img
-                  src="https://i.ibb.co/FhCCvFL/tumblr-3d1ccf3836410d8c14452691a5485a10-6d0d8cbe-540.jpg"
-                  alt="tumblr-3d1ccf3836410d8c14452691a5485a10-6d0d8cbe-540"
-                  border="0"
-                />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title align-item right justify-content-right">
-                    Welcome Captain!
-                  </h5>
-                  <p className="card-text align-items-right justify-content-right">
-                    This is the Elysian Realm Guide! Here, you will learn about
-                    the best characters, weapons, and signets that will aid you
-                    in your success! There are many layers and difficulties ones
-                    will face, and to be prepared you research. In this guide,
-                    you will read about three Valkyries, multiples and their
-                    abilities, and a handful of signets and their properties.
-                  </p>
-                </div>
+          <div className="jumbotron jumbotron-fluid">
+            <div className="carousel-inner">
+              <img
+                className="card-img-top"
+                src="https://i.ibb.co/FhCCvFL/tumblr-3d1ccf3836410d8c14452691a5485a10-6d0d8cbe-540.jpg"
+                alt="tumblr-3d1ccf3836410d8c14452691a5485a10-6d0d8cbe-540"
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="carousel-caption d-flex h-75 align-item left justify-content-left">
+                  Welcome Captain!
+                </h5>
+                <p className="lead carousel-caption d-flex h-50 align-items-left justify-content-left">
+                  This is the Elysian Realm Guide! Here, you will learn about
+                  the best characters, weapons, and signets that will aid you in
+                  your success! There are many layers and difficulties ones will
+                  face, and to be prepared you research. In this guide, you will
+                  read about three Valkyries, multiples and their abilities, and
+                  a handful of signets and their properties.
+                </p>
               </div>
             </div>
           </div>
