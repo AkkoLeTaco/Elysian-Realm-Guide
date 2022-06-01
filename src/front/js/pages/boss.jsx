e96259c89e0f2def9b1d46bc0e229eb08f4354c9
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-06-01 04:12:43 +0000

    change

--- src/front/js/pages/boss.jsx
+++ src/front/js/pages/boss.jsx
@@ -60,7 +60,7 @@ export const Boss = (props) => {
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h5 className="card-title">Mobius</h5>
                   <p className="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
